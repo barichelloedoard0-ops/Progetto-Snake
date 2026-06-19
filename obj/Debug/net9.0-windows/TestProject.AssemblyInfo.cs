6301603d8520a469660c19ed53856731da9e11d1
Bari <barichelloedoard0@gmail.com>
2026-06-19 17:31:50 +0200

    Aggiornamento dei record con implementazione del db

--- obj/Debug/net9.0-windows/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b925ddfbea9bf63c12f006240bd6a88f3bcf91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d140db047be65a5afe3e53a9ba93529b3847f23b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
